0a6cdd49178b88bf0ae177658bde9240858879d4
lpopivnyak <stadnykroman2010@gmail.com>
2024-02-24 19:26:36 +0200

    fotoshop1

--- main.py
+++ main.py
@@ -16,12 +16,28 @@ qest_lbl = QLabel()
 
 answer_list = QListWidget()
 
+mine_line = QHBoxLayout()
+mine_line1= QHBoxLayout()
 
+h1 = QVBoxLayout()
+h1.addWidget(qest_btn)
+h1.addWidget(answer_list)
+mine_line.addLayout(h1)
 
+v2 = QVBoxLayout()
+v2.addWidget(qest_lbl)
+h2 = QHBoxLayout()
+h2.addWidget(qest_lbl)
+h2.addLayout(mine_line1)
+h2.addWidget(qest_btn1)
+h2.addWidget(qest_btn2)
+h2.addWidget(qest_btn3)
+h2.addWidget(qest_btn4)
+h2.addWidget(qest_btn5)
+v2.addLayout(h2)
+mine_line.addLayout(v2)
 
-
-
-
+window.setLayout(mine_line)
 
 
 
