e08b216d7d030f5208ca80dd17f1a367256cdd74
vladysalv077 <stadnykroman2010@gmail.com>
2024-03-16 19:35:57 +0200

    fotoshop123

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import json
+import os
+
+from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtWidgets import *
+from PIL import Image
 
 app = QApplication([])
 
@@ -104,16 +108,52 @@ window.setLayout(mine_line)
 
 
 
+def pil2pixmap(im):
+    if im.mode == "RGB":
+        r, g, b = im.split()
+        im = Image.merge("RGB", (b, g, r))
+    elif im.mode == "RGBA":
+        r, g, b, a = im.split()
+        im = Image.merge("RGBA", (b, g, r, a))
+    elif im.mode == "L":
+        im = im.convert("RGBA")
+    im2 = im.convert("RGBA")
+    data = im2.tobytes("raw", "RGBA")
+    qim = QImage(data, im.size[0], im.size[1], QImage.Format_ARGB32)
+    pixmap = QPixmap.fromImage(qim)
+    return pixmap
 
 
+class Bublik:
+    def __int__(self):
+        self.image = None
+        self.folder = None
+        self.image_name = None
 
+    def load(self):
+        full_path = os.path.join(self.folder,self.image_name)
+        self.image = Image.open(full_path)
+    def show_image(self):
+        pixel = pil2pixmap(self.image)
+        qest_lbl.setPixmap(pixel)
 
+bublik = Bublik()
 
+def show_directory():
+    bublik.folder = QFileDialog.getExistingDirectory()
+    list_files = os.listdir(bublik.folder)
+    answer_list.clear()
+    for file in list_files:
+        if file.endswith("png"):
+             answer_list.addItem(file)
 
+def show_foto():
+    image_name =answer_list.currentItem().text()
+    bublik.image_name = image_name
+    bublik.load()
+    bublik.show_image()
 
-
-
-
-
+answer_list.currentRowChanged.connect(show_foto)
+qest_btn.clicked.connect(show_directory)
 window.show()
 app.exec()
