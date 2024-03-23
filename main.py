cad5994b380e604d0b4b7ba68822f8b78eaca9d6
vladysalv077 <stadnykroman2010@gmail.com>
2024-03-23 19:32:55 +0200

    0987654

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 
 from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtWidgets import *
-from PIL import Image
+from PIL import Image, ImageFilter, ImageEnhance
 
 app = QApplication([])
 
@@ -78,6 +78,9 @@ qest_btn2 = QPushButton("Вправо")
 qest_btn3 = QPushButton("Дзеркально")
 qest_btn4 = QPushButton("Різкість")
 qest_btn5 = QPushButton("Ч/Б")
+qest_btn6 = QPushButton("Контури")
+qest_btn7 = QPushButton("Тиснення")
+qest_btn8 = QPushButton("Яскравість")
 
 qest_lbl = QLabel()
 
@@ -101,6 +104,9 @@ h2.addWidget(qest_btn2)
 h2.addWidget(qest_btn3)
 h2.addWidget(qest_btn4)
 h2.addWidget(qest_btn5)
+h2.addWidget(qest_btn6)
+h2.addWidget(qest_btn7)
+h2.addWidget(qest_btn8)
 v2.addLayout(h2)
 mine_line.addLayout(v2)
 
@@ -137,6 +143,38 @@ class Bublik:
         pixel = pil2pixmap(self.image)
         qest_lbl.setPixmap(pixel)
 
+    def dzrekalo(self):
+        self.image = self.image.transpose(Image.FLIP_TOP_BOTTOM)
+        self.show_image()
+
+    def black_white(self):
+        self.image = self.image.convert("L")
+        self.show_image()
+
+    def rizkist(self):
+        self.image = self.image.filter(ImageFilter.SHARPEN)
+        self.show_image()
+
+    def left(self):
+        self.image = self.image.transpose(Image.ROTATE_90)
+        self.show_image()
+
+    def right(self):
+        self.image = self.image.transpose(Image.FLIP_LEFT_RIGHT)
+        self.show_image()
+
+    def kontur(self):
+        self.image = self.image.filter(ImageFilter.CONTOUR)
+        self.show_image()
+
+    def embossing(self):
+        self.image = self.image.filter(ImageFilter.EMBOSS)
+        self.show_image()
+
+    def light(self):
+        self.image = ImageEnhance.Brightness(self.image).enhance(1.5)
+        self.show_image()
+
 bublik = Bublik()
 
 def show_directory():
@@ -147,13 +185,23 @@ def show_directory():
         if file.endswith("png"):
              answer_list.addItem(file)
 
+
 def show_foto():
     image_name =answer_list.currentItem().text()
     bublik.image_name = image_name
     bublik.load()
     bublik.show_image()
 
+
 answer_list.currentRowChanged.connect(show_foto)
 qest_btn.clicked.connect(show_directory)
+qest_btn3.clicked.connect(bublik.dzrekalo)
+qest_btn5.clicked.connect(bublik.black_white)
+qest_btn4.clicked.connect(bublik.rizkist)
+qest_btn1.clicked.connect(bublik.left)
+qest_btn2.clicked.connect(bublik.right)
+qest_btn6.clicked.connect(bublik.kontur)
+qest_btn7.clicked.connect(bublik.embossing)
+qest_btn8.clicked.connect(bublik.light)
 window.show()
 app.exec()
